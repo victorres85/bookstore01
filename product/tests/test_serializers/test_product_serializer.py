dcabd926c11bd67b9eea0bfef50b9b51c37ca133
Victo Almeida <victorres.almeida@gmail.com>
2022-06-26 01:25:14 +0100

    change for winestore01

--- product/tests/test_serializers/test_product_serializer.py
+++ product/tests/test_serializers/test_product_serializer.py
@@ -6,15 +6,15 @@ from product.serializers import ProductSerializer
 
 class TestProductSerializer(TestCase):
     def setUp(self) -> None:
-        self.category = CategoryFactory(title="technology")
+        self.category = CategoryFactory(wine_name="technology")
         self.product_1 = ProductFactory(
-            title="mouse", price=100, category=[self.category]
+            wine_name="mouse", price=100, category=[self.category]
         )
         self.product_serializer = ProductSerializer(self.product_1)
 
     def test_product_serializer(self):
         serializer_data = self.product_serializer.data
         self.assertEquals(serializer_data["price"], 100)
-        self.assertEquals(serializer_data["title"], "mouse")
+        self.assertEquals(serializer_data["wine_name"], "mouse")
         self.assertEquals(
-            serializer_data["category"][0]["title"], "technology")
+            serializer_data["category"][0]["wine_name"], "technology")

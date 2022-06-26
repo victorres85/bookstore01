b55d9a3f6523c44c63c6bb399083be6f42e85bbd
Victo Almeida <victorres.almeida@gmail.com>
2022-06-26 01:19:26 +0100

    change for winestore01

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Category, Wine
+from .models import Product, Category
 
 
 admin.site.register(Product)

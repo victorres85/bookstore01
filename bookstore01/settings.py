2b6157829e7eee61b60f32dde0da8df5dca31947
Victo Almeida <victorres.almeida@gmail.com>
2022-06-27 21:05:54 +0100

    test api 05

--- bookstore01/settings.py
+++ bookstore01/settings.py
@@ -44,15 +44,15 @@ INSTALLED_APPS = [
     'product',
     'debug_toolbar',
     'rest_framework.authtoken',
-    "corsheaders",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',

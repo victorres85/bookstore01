1a5c882c104e9dde80dc772a6414e8928339e363
Victo Almeida <victorres.almeida@gmail.com>
2022-06-27 08:28:07 +0100

    commit 03

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from product.models.product import Product
+from product.models.product import Product, Category
+from product.serializers.category_serializer import CategorySerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True, many=True)
+    categories_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), write_only=True, many=True
+    )
+
     class Meta:
         model = Product
         fields = [
@@ -20,6 +26,8 @@ class ProductSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
+        category_data = validated_data.pop("categories_id")
+
         product = Product.objects.create(**validated_data)
         for category in category_data:
             product.category.add(category)

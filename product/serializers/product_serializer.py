073ec9c30b2741dc0f5c221d7c531bae6fb0eb46
Victo Almeida <victorres.almeida@gmail.com>
2022-06-27 13:49:01 +0100

    winestore db1

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -14,6 +14,7 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = [
             "id",
+            "vintage",
             "wine_producer",
             "country",
             "wine_name",
